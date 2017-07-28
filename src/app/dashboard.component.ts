68d02a04b603854a7b1a19a0bdab2caf0991cb52
Welch <Cameron.Welch@sogeti.com>
2017-07-28 14:24:56 -0500

    Final Draft

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -25,4 +25,22 @@ export class DashboardComponent implements OnInit, DoCheck {
     this.cubicleService.getCubicles()
       .then(cubicles => this.cubicles = cubicles.slice(0));
   }
+
+  //Determines what day of the week it is
+  findDay(): String {
+    if(this.today==0)
+      return "Sunday";
+    if(this.today==1)
+      return "Monday";
+    if(this.today==2)
+      return "Tuesday";
+    if(this.today==3)
+      return "Wednesday";
+    if(this.today==4)
+      return "Thursday";
+    if(this.today==5)
+      return "Friday";
+    if(this.today==6)
+      return "Saturday";
+  }
 }
